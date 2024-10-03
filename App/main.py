2318b9a1dc4cc5333e8f497d89fab223d31be6ec
Artem Mikhailov <108790381+F1TU3S17@users.noreply.github.com>
2024-10-03 20:46:25 +0300

    add catch NETWORKEROR

--- App/main.py
+++ App/main.py
@@ -1,32 +1,24 @@
 import asyncio
+from aiogram import exceptions
 from aiogram import Bot, Dispatcher
 from aiogram.methods import DeleteWebhook
 from handlers import router
 from handler_admin import router_admin_panel
 import database as db
-from config import Config, load_config
-
-from environs import Env
-
-env = Env()
-env.read_env()
 
 async def on_startup():
     await db.db_start()
 
 async def main():
-
-    config: Config = load_config()
-    bot = Bot(token=config.tg_bot.token)
-    dp = Dispatcher()
-
-    dp.include_routers(router, router_admin_panel)
-    dp.startup.register(on_startup)
-    
-    await bot(DeleteWebhook(drop_pending_updates=True))
-    await dp.start_polling(bot)
-
-
+    try:
+        bot = Bot(token='6971765827:AAGGYCGi7ouJ_0A5ZKwFX-VPbm5Cl_sSI68')
+        dp = Dispatcher()
+        dp.include_routers(router, router_admin_panel)
+        dp.startup.register(on_startup)
+        await bot(DeleteWebhook(drop_pending_updates=True))
+        await dp.start_polling(bot)
+    except exceptions.TelegramNetworkError as e:
+        print(f"Network error: {e}")
 
 if __name__ == '__main__':
     try:
